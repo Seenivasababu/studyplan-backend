ddd6c8ef4cc4170b3ca1c0492777c916b7188a80
Seenivasababu <120184836+Seenivasababu@users.noreply.github.com>
2023-08-01 17:27:15 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user')
+const User = require('../models/User')
 
 // User registration API
 router.post('/signup', async (req, res) => {
